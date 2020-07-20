16ed8fcecef45942654c4702441e7530208149d6
naerim <naerim1119@gmail.com>
2020-07-21 00:20:31 +0900

    [ADD] npm i styled-components @types/styled-components

--- todo_typescript/src/App.tsx
+++ todo_typescript/src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+// npm i styled-components @types/styled-components
 
 function App() {
-  return <div>Hello World!</div>;
+  return <TodoTemplate>Hello World!</TodoTemplate>;
 }
 
+const TodoTemplate = styled.div`
+  width: 512px;
+`;
+
 export default App;
-
-
